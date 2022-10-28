635cf2185c9ed200c453d7a817cb76fef5b4a6c1
Richard Phillis <rphillis@studiosity.com>
2022-10-28 10:05:03 +1000

    Disable debug mode

--- run_powa.py
+++ run_powa.py
@@ -5,7 +5,7 @@ from tornado.options import options
 import logging
 
 if __name__ == "__main__":
-    application = make_app(debug=True, gzip=True, compress_response=True)
+    application = make_app(debug=False, gzip=True, compress_response=True)
     application.listen(8888)
     logger = logging.getLogger("tornado.application")
     logger.info("Starting powa-web on http://127.0.0.1:8888%s",
